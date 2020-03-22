3657a07d5478559a975feaa2d8acb88119cd7409
agapkinadiana <agapkinadiana@mail.ru>
2020-03-22 22:30:43 +0300

    feat: add solution for 6 task

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,20 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    
+    NSUInteger conditionNumber = [number intValue];
+    
+    NSUInteger fibPrevious = 0;
+    NSUInteger fibCurrent = 1;
+    
+    while (fibPrevious * fibCurrent < conditionNumber) {
+        NSUInteger fibSum = fibPrevious + fibCurrent;
+        fibPrevious = fibCurrent;
+        fibCurrent = fibSum;
+    }
+    
+    NSArray *result = @[@(fibPrevious), @(fibCurrent), (fibPrevious * fibCurrent == conditionNumber) ? @(1) : @(0)];
+    
+    return result;
 }
 @end
